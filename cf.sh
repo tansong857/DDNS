0c7492df0f4feded811b95b40bd46974c2435799
Tobias <tansong857@gmail.com>
2023-11-08 21:14:16 +0800

    cf.sh

---
+++ cf.sh
@@ -0,0 +1,27 @@
+# Download the cf-v4-ddns.sh file
+cd /root
+
+# Cloudflare API key
+CFKEY="UYwNse0t0KEW3Zj012DUa-drA8aNEzLG1LtP7iOw"
+
+wget -q https://raw.githubusercontent.com/jamespan2012/myshellscripts/main/cf-v4-ddns.sh -O cf-v4-ddns.sh
+
+
+# Prompt the user to enter the CFZONE_NAME value
+read -p "Enter Cloudflare Zone name: " CFZONE_NAME
+
+# Prompt the user to enter the CFRECORD_NAME value
+read -p "Enter Cloudflare Record name: " CFRECORD_NAME
+
+# Change the values in the cf-v4-ddns.sh file
+sed -i "s/aCFKEY=.*/CFKEY=\"$CFKEY\"/" cf-v4-ddns.sh
+sed -i "s/aCFZONE_NAME=.*/CFZONE_NAME=\"$CFZONE_NAME\"/" cf-v4-ddns.sh
+sed -i "s/aCFRECORD_NAME=.*/CFRECORD_NAME=\"$CFRECORD_NAME\"/" cf-v4-ddns.sh
+
+echo "Updated values in cf-v4-ddns.sh successfully!"
+
+echo "*/1 * * * * /root/cf-v4-ddns.sh >/dev/null 2>&1" >> mycron
+crontab mycron
+rm mycron
+
+echo "Cron successfully!"
