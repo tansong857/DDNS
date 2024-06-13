73e066f141bf6f3e409ad7638f8d590674df995c
Tobias <tansong857@gmail.com>
2024-06-14 01:43:00 +0800

    cf.sh

--- cf.sh
+++ cf.sh
@@ -9,6 +9,12 @@ CFKEY="UYwNse0t0KEW3Zj012DUa-drA8aNEzLG1LtP7iOw"
 # 下载 cf-v4-ddns.sh 文件
 wget -q https://raw.githubusercontent.com/jamespan2012/myshellscripts/main/cf-v4-ddns.sh -O cf-v4-ddns.sh
 
+# 确保脚本文件存在且已下载成功
+if [[ ! -f "cf-v4-ddns.sh" ]]; then
+  echo "cf-v4-ddns.sh 文件下载失败。"
+  exit 1
+fi
+
 # 提示用户输入 CFZONE_NAME 值
 read -p "输入 Cloudflare 区域名称：" CFZONE_NAME
 
