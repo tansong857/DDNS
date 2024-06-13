d02c6ee4b2864a48320280e0365cbb0630c220fd
Tobias <tansong857@gmail.com>
2024-06-14 01:40:21 +0800

    cf.sh

--- cf.sh
+++ cf.sh
@@ -1,27 +1,34 @@
-# Download the cf-v4-ddns.sh file
-cd /root
+#!/bin/bash
 
-# Cloudflare API key
+# 进入根目录
+cd /
+
+# 设置 Cloudflare API 密钥
 CFKEY="UYwNse0t0KEW3Zj012DUa-drA8aNEzLG1LtP7iOw"
 
+# 下载 cf-v4-ddns.sh 文件
 wget -q https://raw.githubusercontent.com/jamespan2012/myshellscripts/main/cf-v4-ddns.sh -O cf-v4-ddns.sh
 
+# 提示用户输入 CFZONE_NAME 值
+read -p "输入 Cloudflare 区域名称：" CFZONE_NAME
 
-# Prompt the user to enter the CFZONE_NAME value
-read -p "Enter Cloudflare Zone name: " CFZONE_NAME
+# 提示用户输入 CFRECORD_NAME 值
+read -p "输入 Cloudflare 记录名称：" CFRECORD_NAME
 
-# Prompt the user to enter the CFRECORD_NAME value
-read -p "Enter Cloudflare Record name: " CFRECORD_NAME
+# 更改 cf-v4-ddns.sh 文件中的值
+sed -i "s/^CFKEY=.*/CFKEY=\"$CFKEY\"/" cf-v4-ddns.sh
+sed -i "s/^CFZONE_NAME=.*/CFZONE_NAME=\"$CFZONE_NAME\"/" cf-v4-ddns.sh
+sed -i "s/^CFRECORD_NAME=.*/CFRECORD_NAME=\"$CFRECORD_NAME\"/" cf-v4-ddns.sh
 
-# Change the values in the cf-v4-ddns.sh file
-sed -i "s/aCFKEY=.*/CFKEY=\"$CFKEY\"/" cf-v4-ddns.sh
-sed -i "s/aCFZONE_NAME=.*/CFZONE_NAME=\"$CFZONE_NAME\"/" cf-v4-ddns.sh
-sed -i "s/aCFRECORD_NAME=.*/CFRECORD_NAME=\"$CFRECORD_NAME\"/" cf-v4-ddns.sh
+echo "cf-v4-ddns.sh 中的值已成功更新！"
 
-echo "Updated values in cf-v4-ddns.sh successfully!"
+# 添加执行权限
+chmod +x cf-v4-ddns.sh
 
-echo "*/1 * * * * /root/cf-v4-ddns.sh >/dev/null 2>&1" >> mycron
-crontab mycron
-rm mycron
+# 执行 cf-v4-ddns.sh 脚本
+bash cf-v4-ddns.sh
 
-echo "Cron successfully!"
+# 添加定时任务到 crontab
+(crontab -l ; echo "*/1 * * * * /root/cf-v4-ddns.sh >/dev/null 2>&1") | crontab -
+
+echo "Cron成功！"
